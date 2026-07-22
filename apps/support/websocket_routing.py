9eb206352b0925ad8d8712e6080fe45823ce2bd3
webxazim <webxazim@gmail.com>
2026-07-22 21:35:36 +0300

    obsolete delete

--- apps/support/websocket_routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from apps.support.consumers import SupportTeamConsumer, SupportWidgetConsumer
-
-websocket_urlpatterns = [
-    path("ws/support/", SupportTeamConsumer.as_asgi()),
-    path("ws/support/widget/<uuid:site_key>/", SupportWidgetConsumer.as_asgi()),
-]
